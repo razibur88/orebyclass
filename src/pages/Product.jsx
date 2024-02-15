8577a83907a5e2e729313fe1035ddc10c39285eb
Shohelrana63 <shohelrana.beg@gmail.com>
2024-02-15 19:22:02 +0600

    demo

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Container from '../components/Container'
 import { Link } from 'react-router-dom'
 import Flex from '../components/Flex'
-import Pimag from "../assets/im1.png"
-import { FaCartPlus,FaHeart } from "react-icons/fa";
-import { IoGitCompare } from "react-icons/io5";
 import { Apidata } from '../components/ContextApi'
+import Post from '../components/pagination/Post'
+import PaginationNum from '../components/pagination/PaginationNum'
 
 const Product = () => {
     let data = useContext(Apidata)
+    let [currentPage, setCurrentPage] = useState(1)
+    let [perPage, setPerPage] = useState(6)
+
+    let lastPage = currentPage * perPage
+    let firstPage = lastPage - perPage
+    
+    let allPage = data.slice(firstPage, lastPage)
+
+
+    let paginate = (pageNumber)=>{
+        setCurrentPage(pageNumber)
+    }
+
+    let next = () =>{
+       if(currentPage < data.length){
+        setCurrentPage((state)=> state + 1)
+       }
+    }
+
+    let prve = () =>{
+        if(currentPage > 1){
+            setCurrentPage((state)=> state - 1)
+        }
+    }
   return (
     <div>
         <Container>
@@ -19,31 +42,9 @@ const Product = () => {
                 </div>
                 <div className="w-[80%]">
                     <div className="flex justify-between flex-wrap">
-                        {data.map((item)=>(
-                            <div className='w-[32%]'>
-                            <div className="py-3">
-                            <div className="relative group">
-                                <img src={Pimag} className='h-[300px] w-full' alt="" />
-                                <div className="absolute bottom-0 overflow-y-hidden left-0 w-[100%] duration-300 ease-in bg-[#FFFFFF] h-[0px] opacity-0 group-hover:h-[200px] group-hover:opacity-100">
-                                    <div className=" flex items-center h-full justify-end">
-                                        <ul className='px-6 text-end'>
-                                            <li className=''>Add to Wish List <FaHeart className='inline-block' /> </li>
-                                            <li className=''>Compare <IoGitCompare className='inline-block'/></li>
-                                            <li className=''>Add to Cart <FaCartPlus className='inline-block'/></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex justify-between pt-3">
-                                <h3 className='font-dm font-bold text-[16px]'>Kicu akta</h3>
-                                <p>$00</p>
-                            </div>
-                            </div>
+                        <Post post={allPage}/>
                     </div>
-                        ))}
-                    
-                    
-                    </div>
+                    <PaginationNum totalPage={data.length} perPage={perPage} paginate={paginate} next={next} prve={prve} currentPage={currentPage}/>
                 </div>
             </Flex>
         </Container>
