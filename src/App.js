93c6eaeb716769b7def493c8c8ed91bd30d27e2a
wasimmahamod <naimak659@gmail.com>
2024-03-12 16:42:59 +0600

    demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import RootLayout from "./components/root/RootLayout";
 import Home from "./pages/Home";
-import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Product from "./pages/Product";
 import Error from "./pages/Error";
 import ProductDetails from "./pages/ProductDetails";
@@ -8,18 +13,26 @@ import Cart from "./pages/Cart";
 import Aboutbrand from "./pages/About";
 import Contact from "./pages/Contact";
 import Checkout from "./pages/Checkout";
-let router = createBrowserRouter(createRoutesFromElements(
-  <Route element={<RootLayout/>}>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/product" element={<Product/>}></Route>
-    <Route path="/product/:id" element={<ProductDetails/>}></Route>
-    <Route path="/cart" element={<Cart/>}></Route>
-    <Route path="/about" element={<Aboutbrand/>}></Route>
-    <Route path="/contact" element={<Contact/>}></Route>
-    <Route path="/checkout" element={<Checkout/>}></Route>
-    <Route path="*" element={<Error/>}></Route>
-  </Route>
-))
+import Myaccount from "./pages/Myaccount";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+let router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />}>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/product" element={<Product />}></Route>
+      <Route path="/product/:id" element={<ProductDetails />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+      <Route path="/about" element={<Aboutbrand />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/checkout" element={<Checkout />}></Route>
+      <Route path="/account" element={<Myaccount />}></Route>
+      <Route path="/sign" element={<Signup />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="*" element={<Error />}></Route>
+    </Route>
+  )
+);
 function App() {
   return (
     <>
